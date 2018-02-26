34a610b9897d3e3ec6d7a98cb93daa6df7188c71
Khuyen Duong <truckhuyen@gmail.com>
2018-02-26 06:00:36 +0000

    Done basic functions

--- chromosome.go
+++ chromosome.go
@@ -1,38 +1,82 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
 )
 
 type Chromosome struct {
-    gene string
+	gene string
 }
 
+/*
+ "abc" => "011000010110001001100011"
+*/
+func stringToGene(s string) string {
+	res := ""
+	for _, c := range s {
+		res = fmt.Sprintf("%s%.8b", res, c)
+	}
+	return res
+}
+
+/*
+ "011000010110001001100011" => "abc"
+*/
+func geneToString(s string) string {
+	var ans []string = make([]string, len(s)/8)
+	for i := 0; i < len(s)/8; i++ {
+		a, b := i*8, (i+1)*8
+		tmp := s[a:b]
+		x, err := strconv.ParseInt(tmp, 2, 64)
+		if err == nil {
+			ans = append(ans, string(x))
+		}
+
+	}
+	return strings.Join(ans, "")
+}
+
+/*
+ See how far the current string is from the target string
+*/
+func getScore(chr Chromosome, target string) float64 {
+	ans := float64(0)
+	tmp := geneToString(chr.gene)
+	for i := range target {
+		ans += math.Abs(float64(target[i]) - float64(tmp[i]))
+	}
+	if ans == 0 {
+		return 1.0
+	} else {
+		return 1.0 / float64(ans)
+	}
+}
+
+/*
+ Flip one random bit along the gene as to mutate
+*/
 func mutate(chr Chromosome, rate float64) Chromosome {
-    var index int = int( rate * float64(len(chr.gene) - 1) )
-    var rep string = string(int('1') - int(chr.gene[index]) + int('0'))
-    var ans string =
-            chr.gene[0:index] + rep + chr.gene[index+1:len(chr.gene)]
-    return Chromosome{ans}
+	var index int = int(rate * float64(len(chr.gene)-1))
+	var rep string = string(int('1') - int(chr.gene[index]) + int('0'))
+	var ans string = chr.gene[0:index] + rep + chr.gene[index+1:len(chr.gene)]
+	return Chromosome{ans}
 }
 
+/*
+ Create a new child from 2 parents
+*/
 func crossOver(pa1 Chromosome, pa2 Chromosome, rate float64) []Chromosome {
-    var index int = int( rate * float64(len(pa1.gene) - 1) )
-    
-    var child1 Chromosome = 
-        Chromosome{pa1.gene[0:index] + pa2.gene[index+1:len(pa2.gene)]}
-    var child2 Chromosome = 
-        Chromosome{pa2.gene[0:index] + pa1.gene[index+1:len(pa1.gene)]}
-        
-    // fmt.Printf(
-    //     "%s & %s ==> %s, %s\n", pa1.gene, pa2.gene, child1.gene, child2.gene)
-        
-    return []Chromosome{child1, child2}
+	var index int = int(rate * float64(len(pa1.gene)-1))
+
+	var child1 Chromosome = Chromosome{pa1.gene[0:index] + pa2.gene[index+1:len(pa2.gene)]}
+	var child2 Chromosome = Chromosome{pa2.gene[0:index] + pa1.gene[index+1:len(pa1.gene)]}
+
+	return []Chromosome{child1, child2}
 }
 
 func main() {
-    var c1 Chromosome = Chromosome{"101"}
-    mutate(c1, 0.7)
-    // var c2 Chromosome = Chromosome{"1011010001"}
-    // fmt.Println(crossOver(c1, c2, 0.7))
+
 }
